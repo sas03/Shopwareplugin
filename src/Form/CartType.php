e74aa342bb04b1a81bb2009d8347687e1e9e8d8d
sas03 <stephanesumo@yahoo.fr>
2021-06-28 11:12:32 +0200

    repos

--- src/Form/CartType.php
+++ src/Form/CartType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Order;
+use App\Form\EventListener\ClearCartListener;
+use App\Form\EventListener\RemoveCartItemListener;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -19,6 +21,9 @@ class CartType extends AbstractType
             ])
             ->add('save', SubmitType::class)
             ->add('clear', SubmitType::class);
+
+        $builder->addEventSubscriber(new RemoveCartItemListener());
+        $builder->addEventSubscriber(new ClearCartListener());
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Repository/OrderRepository.php
+++ src/Repository/OrderRepository.php
@@ -47,4 +47,25 @@ class OrderRepository extends ServiceEntityRepository
         ;
     }
     */
+    
+    /**
+     * Finds carts that have not been modified since the given date.
+     *
+     * @param \DateTime $limitDate
+     * @param int $limit
+     *
+     * @return int|mixed|string
+     */
+    public function findCartsNotModifiedSince(\DateTime $limitDate, int $limit = 10): array
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.status = :status')
+            ->andWhere('o.updatedAt < :date')
+            ->setParameter('status', Order::STATUS_CART)
+            ->setParameter('date', $limitDate)
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
